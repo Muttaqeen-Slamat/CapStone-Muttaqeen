1dfb4bfebbb894cb049288a5828f238dbc68dd75
Muttaqeen-Slamat <muttaqeens@gmail.com>
2024-03-08 10:14:16 +0200

    update to config index

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const connection = createPool({
     host: process.env.DB_Host,
     database: process.env.DB_Name,
     user: process.env.DB_UserName,
-    password: process.env.DB_Password,
+    password: process.env.DB_Pass,
     multipleStatements: true,
     connectionLimit: 30
 })
