9f8b7e794b37ad41b5e3c55aef0f6362fe0761df
Muttaqeen-Slamat <muttaqeens@gmail.com>
2024-03-11 10:07:40 +0200

    nav fix

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,12 +2,12 @@
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="/">Logo Placeholder</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav mb-2 ms-auto mb-lg-0">
         <li class="nav-item">
           <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
         </li>
