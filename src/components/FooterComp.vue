c2b77278c62dd0093025b6c7764a8722ccbcdd5b
Muttaqeen-Slamat <muttaqeens@gmail.com>
2024-03-12 09:18:25 +0200

    some changes to the footer component

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,25 +1,26 @@
 <template lang="">
 
-  <footer id="footerID" class="py-3 container-fluid">
+  <footer id="footerID" class="py-4 container-fluid">
     <div class="container">
 
-      <div class="row">
-        <p class="lead">
-          Footer text here &copy;
-          <span>{{ currYear }}</span>
-        </p>
-      </div>
+      
 
-      <div class="row">
+      <div class="row mt-3">
         <div class="col">
-          <p>Col 1 test</p>
+          <p>News Feed</p>
         </div>
 
-        <div class="col">
-          <p>Col 2 test</p>
-          <router-link to="/contact">
+        <div class="col text-decoration-underline">
+          <p>Pages</p>
+          <a href="/about">
+            <p>About</p>
+          </a>
+          <a href="/products">
+            <p>Products</p>
+          </a>
+          <a href="/contact">
             <p>Contact</p>
-          </router-link>
+          </a>
         </div>
 
         <div class="col">
@@ -31,14 +32,20 @@
             <div>
               <p>Email: <span>muttaqeens@gmail.com</span></p>
             </div>
-            <div class="d-flex justify-content-center gap-2">
-              <i class="bi bi-github"></i>
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-person-circle"></i>
+            <div class="d-flex justify-content-center gap-2 rcol">
+              <a href="https://github.com/Muttaqeen-Slamat" target="_blank"><i class="bi bi-github">Github</i></a>
+              <a href="https://www.linkedin.com/in/muttaqeen-slamat-95209019b/" target="_blank"><i class="bi bi-linkedin">Linkedin</i></a>
             </div>
           </div>
         </div>
 
+      </div>
+
+      <div class="row mt-3">
+        <p class="lead">
+          CompanyName &copy;
+          <span>{{ currYear }}</span>
+        </p>
       </div>
 
     </div>
@@ -60,5 +67,22 @@ export default {
   position: absolute;
   background-color: rgb(212, 212, 212);
   color: black;
+  text-decoration-style: none ;
 }
+
+
+i{
+  color: black;
+  text-decoration: underline;
+}
+
+.col2{
+  color: black;
+  text-decoration: none;
+}
+
+p{
+  color: black;
+}
+
 </style>
