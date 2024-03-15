5450afe671cf155edf83d506d6617952fe9631bd
Muttaqeen Slamat <muttaqeens@gmail.com>
2024-03-15 12:08:41 +0200

    update to login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -18,13 +18,27 @@
       </div>
     </div>
     <button type="button" class="btn btn-primary" @click="login">Login</button>
-    <!-- User list -->
-    <div>
-      <h2>All Users</h2>
-      <ul>
-        <li v-for="user in users" :key="user.id">{{ user.firstName }} {{ user.lastName }}</li>
-      </ul>
+    <form>
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
     </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="form-check text-start my-3">
+      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">
+        Remember me
+      </label>
+    </div>
+    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
+  </form>
   </div>
 </template>
 
@@ -56,4 +70,8 @@ export default {
 
 <style scoped>
 /* Add your custom styles here */
+form {
+    display: block;
+    margin-top: 0em;
+}
 </style>
