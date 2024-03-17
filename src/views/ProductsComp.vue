fe29dd467ba3bb8ce09c251dbacf4cf3cd098c6b
Muttaqeen Slamat <muttaqeens@gmail.com>
2024-03-17 12:28:13 +0200

    update tp products with the search

--- src/views/ProductsComp.vue
+++ src/views/ProductsComp.vue
@@ -3,24 +3,20 @@
         <div class="row">
             <h2 class="display-2">Products</h2>
         </div>
-        <div class="row d-flex align-items-center justify-content-center" v-if="product">
-            <Card v-for="product in product" :key="product.prodID">
+        <!-- Search input -->
+        <div class="row">
+            <input type="text" v-model="searchQuery" placeholder="Search products">
+        </div>
+        <!-- Products list -->
+        <div class="row d-flex align-items-center justify-content-center" v-if="filteredProducts.length">
+            <Card v-for="product in filteredProducts" :key="product.prodID">
                 <template #cardHeader>
                     <h4 class="card-title">{{ product.prodName }}</h4>
                 </template>
                 <template #cardBody>
                     <img :src="product.img" :alt="`${ product.prodName }`">
-                    <!-- <p class="card-text text-dark bg-gradient bg-dark-subtle p-2">
-                        Category: {{ product.category }}
-                    </p>
-                    <p class="card-text text-dark bg-gradient bg-dark-subtle p-2">
-                        Description: {{ product.prodDesc }}
-                    </p>
-                    <p class="card-text text-dark bg-gradient bg-dark-subtle p-2">
-                        Quantity: {{ product.quantity }}
-                    </p> -->
                     <p class="card-text text-dark bg-gradient p-2">
-                     R{{ product.prodPrice }}
+                        R{{ product.prodPrice }}
                     </p>
                     <router-link :to="{name: 'product', params: {id: product.prodID}}">View More</router-link>
                 </template>
@@ -34,20 +30,38 @@
 
 <script>
 import Card from '@/components/Card.vue';
-    export default {
-        name: 'ProductsView', 
-        components:{
-            Card
+export default {
+    name: 'ProductsView', 
+    components:{
+        Card
+    },
+    data() {
+        return {
+            searchQuery: '',
+        };
+    },
+    computed: {
+        product() {
+            return this.$store.state.products;
         },
-        computed: {
-            product() {
-                return this.$store.state.products
-            }
-        },
-        mounted() {
-            this.$store.dispatch('fetchProducts')
+        filteredProducts() {
+            // Filter products based on search query
+            return this.product.filter(product =>
+                product.prodName.toLowerCase().includes(this.searchQuery.toLowerCase())
+            );
         }
+    },
+    methods: {
+        sortProducts() {
+            // You can implement sorting logic here
+            // Example: Sort products based on price
+            this.product.sort((a, b) => a.prodPrice - b.prodPrice);
+        }
+    },
+    mounted() {
+        this.$store.dispatch('fetchProducts');
     }
+}
 </script>
 
 <style scoped>
