fd765d4b5d4d25414b46279d61ddfe402d1e8bd2
Muttaqeen-Slamat <muttaqeens@gmail.com>
2024-03-19 10:19:15 +0200

    admin shows for only admins

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,49 +1,52 @@
 <template>
-    <div>
-        <nav class="navbar navbar-expand-lg bg-body-secondary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Logo Placeholder</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mb-2 ms-auto mb-lg-0">
-        <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/about">About</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/admin">Admin </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/products">Products</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/signup">Signup</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/login">Login</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/contact">Contact</router-link>
-        </li>
-        <li class="nav-item profile">
-            <button v-if="loggedInUser" class="nav-link active profile text-light" @click="goToProfile" aria-current="page">{{ loggedInUser.firstName }} {{ loggedInUser.lastName }}</button>
-          </li>
-      </ul>
-    </div>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-secondary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Logo Placeholder</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mb-2 ms-auto mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/about">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/products">Products</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/contact">Contact</router-link>
+            </li>
+            <li class="nav-item" v-if="isAdmin">
+              <router-link class="nav-link active" aria-current="page" to="/admin">Admin</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/signup">Signup</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/login">Login</router-link>
+            </li>
+            <li class="nav-item profile">
+              <button v-if="loggedInUser" class="nav-link active profile text-light" @click="goToProfile" aria-current="page">{{ loggedInUser.firstName }} {{ loggedInUser.lastName }}</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
-</nav>
-    </div>
 </template>
 
 <script>
-    export default {
-      computed: {
+export default {
+  computed: {
     loggedInUser() {
       return this.$store.state.user || [];
+    },
+    isAdmin() {
+      return this.loggedInUser && this.loggedInUser.userRole === "Admin";
     }
   },
   methods: {
@@ -68,7 +71,7 @@
       }
     }
   }
-    }
+}
 </script>
 
 <style scoped>
